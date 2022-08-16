710b418dede49e825438d5a9860a788206e0a817
abbasguliyev <abbasquliyev111@gmail.com>
2022-08-16 11:50:55 +0400

    cors_allow_headers

--- kodaze/core/settings.py
+++ kodaze/core/settings.py
@@ -239,6 +239,17 @@ USE_TZ = True
 # CORS conf
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 
 # Static files (CSS, JavaScript, Images)
