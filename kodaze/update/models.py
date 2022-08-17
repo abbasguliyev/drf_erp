969c90d2c23325877947afcbb8802ebb0587b540
abbasguliyev <abbasquliyev111@gmail.com>
2022-08-17 23:50:54 +0400

    update app added

--- kodaze/update/models.py
+++ kodaze/update/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Update(models.Model):
     update_name = models.CharField(max_length=150)
-    update_description = models.CharField(max_length=250)
+    update_description = models.TextField()
     update_version = models.CharField(max_length=100)
 
     class Meta:
