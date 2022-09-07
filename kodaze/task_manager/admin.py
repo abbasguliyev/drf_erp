4e2f928a9d3b0107d517b6163fe5763c3658e7d2
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-07 16:54:04 +0400

    task manager admin panel

--- kodaze/task_manager/admin.py
+++ kodaze/task_manager/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import TaskManager, UserTaskRequest
 # Register your models here.
+class TaskManagerAdmin(admin.ModelAdmin):
+    list_filter = [
+        "title",
+        'created_date',
+        'end_date',
+        'position',
+        'employee',
+        'type',
+        'status',
+    ]
+    search_fields = (
+        "employee__asa",
+    )
 
-admin.site.register(TaskManager)
+admin.site.register(TaskManager, TaskManagerAdmin)
 admin.site.register(UserTaskRequest)
