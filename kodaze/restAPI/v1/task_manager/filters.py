46eaf9b189b624b1a806330215989d5a5dbe8cf8
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-15 18:47:01 +0400

    Task manager filter date bugs fixed

--- kodaze/restAPI/v1/task_manager/filters.py
+++ kodaze/restAPI/v1/task_manager/filters.py
@@ -2,9 +2,9 @@ import django_filters
 from task_manager.models import TaskManager
 
 class TaskManagerFilter(django_filters.FilterSet):
-    date = django_filters.DateFilter(field_name='date', input_formats=["%d-%m-%Y"])
-    date__gte = django_filters.DateFilter(field_name='date', lookup_expr='gte', input_formats=["%d-%m-%Y"])
-    date__lte = django_filters.DateFilter(field_name='date', lookup_expr='lte', input_formats=["%d-%m-%Y"])
+    date = django_filters.DateTimeFilter(field_name='date', input_formats=["%d-%m-%Y %H:%M:%S"])
+    date__gte = django_filters.DateTimeFilter(field_name='date', lookup_expr='gte', input_formats=["%d-%m-%Y %H:%M:%S"])
+    date__lte = django_filters.DateTimeFilter(field_name='date', lookup_expr='lte', input_formats=["%d-%m-%Y %H:%M:%S"])
     
     class Meta:
         model = TaskManager
