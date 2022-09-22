22526d051b0dbfe0341b8cc90d261ab56c6bfe29
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-22 18:50:30 +0400

    task manager

--- kodaze/core/celeryconfig.py
+++ kodaze/core/celeryconfig.py
@@ -111,6 +111,6 @@ app.conf.beat_schedule = {
     },
     "manage_task_manager": {
         "task": "manage_task_manager",
-        "schedule": crontab(hour='*/5'),
+        "schedule": crontab(hour="2", minute="0"),
     },
 }
