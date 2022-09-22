a83f12974672f132fd472603654e1c95756ca2e4
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-22 18:43:32 +0400

    task manager

--- kodaze/task_manager/tasks.py
+++ kodaze/task_manager/tasks.py
@@ -9,10 +9,8 @@ def manage_task_manager():
     tasks = TaskManager.objects.select_related(
         'position', 'employee').filter(status="İcra edilir")
     for task in tasks:
-        d1 = datetime.datetime(task.end_date.year, task.end_date.month,
-                            task.end_date.day, task.end_date.hour, task.end_date.minute, task.end_date.second)
-        d2 = datetime.datetime(indi.year, indi.month,
-                            indi.day, indi.hour, indi.minute, indi.second)
+        d1 = datetime.date(task.end_date.year, task.end_date.month, task.end_date.day)
+        d2 = datetime.date.today()
         if d1 >= d2:
             task.status = "Gecikir"
             task.save()
