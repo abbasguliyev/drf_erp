c5e993af4885d8182e7a9f20d96dea240145552d
abbasguliyev <abbasquliyev111@gmail.com>
2022-09-28 19:08:39 +0400

    contract and product

--- kodaze/core/settings.py
+++ kodaze/core/settings.py
@@ -313,6 +313,7 @@ CACHES = {
     }
 }
 
+
 CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
 CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
 CELERY_ACCEPT_CONTENT = ['application/json']
