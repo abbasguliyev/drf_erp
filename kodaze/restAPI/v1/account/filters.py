d5a3858d34ab029f5de4ce7bbf729a5dc1aa80d0
abbasguliyev <abbasquliyev111@gmail.com>
2022-10-18 19:25:34 +0400

    company, holding, department, section, position

--- kodaze/restAPI/v1/account/filters.py
+++ kodaze/restAPI/v1/account/filters.py
@@ -38,6 +38,7 @@ class UserFilter(django_filters.FilterSet):
     user_of_companies = django_filters.BooleanFilter(method="user_of_company", label="user_of_companies")
     user_of_departments = django_filters.BooleanFilter(method="user_of_department", label="user_of_departments")
     user_of_offices = django_filters.BooleanFilter(method="user_of_office", label="user_of_offices")
+    user_of_holding = django_filters.BooleanFilter(method="user_of_holding", label="user_of_holding")
 
 
     class Meta:
