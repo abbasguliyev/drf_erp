2c9439a383c2b484663d071413068ef0ac6c8f42
abbasguliyev <abbasquliyev111@gmail.com>
2022-11-25 16:44:15 +0400

    last changes in page of insan resurslari

--- kodaze/core/settings.py
+++ kodaze/core/settings.py
@@ -287,7 +287,7 @@ CACHE_TTL = 60 * 15
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ['CELERY_BROKER_URL'],
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
